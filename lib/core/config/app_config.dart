8d4315b9324d09f0a8a9d2a36da07ded3e9aad0b
Rozan Alawar <rosanalawer2002@gmail.com>
2025-07-30 19:13:05 +0300

    first commit

---
+++ lib/core/config/app_config.dart
@@ -0,0 +1,3 @@
+class AppConfig {
+  static const String appID = "3a8f0a0a04644358b43f49b38a013a6b";
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:streamer/agora_vedio_call_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Live Straeming App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const VedioCallScreen(),
+    );
+  }
+}
